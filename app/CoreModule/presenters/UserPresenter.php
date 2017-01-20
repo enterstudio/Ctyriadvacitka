476edc9c4f13393044ddc3ca3c6c6c06abc94538
krouma <kroupa.matyas@gmail.com>
2017-01-20 19:12:06 +0100

    Users can edit only themselves

--- app/CoreModule/presenters/UserPresenter.php
+++ app/CoreModule/presenters/UserPresenter.php
@@ -54,6 +54,14 @@ class UserPresenter extends BasePresenter {
     public function actionEditor(string $username = NULL){
         //Pokud bylo zadáno jméno, pokusí se uživatele načíst a předat jeho hodnoty do editačního formuláře, jinak vypíše chybovou hlášku
         if ($username) ($user = $this->userManager->getUserByUsername($username)) ? $this['editorForm']->setDefaults($user) : $this->flashMessage('Uživatel nebyl nalezen');
+        if (!$this->user->isLoggedIn()){
+            $this->flashMessage('Nejste přihlášen!');
+            $this->redirect(':Core:Session:signIn');
+        }
+        if ($this->user->id != $user['user_id']){
+            $this->flashMessage('Nemůžete upravovat jiné uživatele!');
+            $this->redirect(':Core:User:', $this->user->getIdentity()->username);
+        }
         $this->template->user = $user;
     }
 
