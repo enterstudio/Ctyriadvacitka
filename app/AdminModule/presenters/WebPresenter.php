77ce33cd70d48a698e76c7a0e6e6288b9d93523e
krouma <kroupa.matyas@gmail.com>
2017-03-05 09:16:27 +0100

    Add backend to web administration

--- app/AdminModule/presenters/WebPresenter.php
+++ app/AdminModule/presenters/WebPresenter.php
@@ -10,6 +10,7 @@ namespace App\AdminModule\Presenters;
 
 
 use App\Presenters\BasePresenter;
+use Nette\Forms\Form;
 
 /**
  * Zpracovává vykreslování administrační sekce
@@ -23,5 +24,39 @@ class WebPresenter extends BasePresenter
         parent::startup();
         $this->logInRequired();
         $this->adminPermissionsRequired();
+    }
+
+    public function renderDefault()
+    {
+        $properties['webName'] = $this->projectManager->getParameter('webName');
+        $properties['webDescription'] = $this->projectManager->getParameter('webDescription');
+        $this['webPropertiesForm']->setDefaults($properties);
+    }
+
+    /**
+     * @return Form
+     */
+    public function createComponentWebPropertiesForm(): Form
+    {
+        $form = $this->formFactory->create();
+        $form->addText('webName', 'Jméno webu')
+            ->setRequired();
+        $form->addText('webDescription', 'Popis webu')
+            ->setRequired();
+        $form->addSubmit('submit', 'Uložit');
+        $form->onSuccess[] = [$this, 'webPropertiesFormSucceeded'];
+        return $form;
+    }
+
+    /**
+     * @param Form $form
+     * @param array $values
+     */
+    public function webPropertiesFormSucceeded(Form $form, array $values)
+    {
+        foreach ($values as $key => $value) {
+            $this->projectManager->saveParameter($key, $value);
+        }
+        $this->flashMessage('Údaje byly upraveny', 'success');
     }
 }
