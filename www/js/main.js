4348f5f2dcb6b8cbccce1da0e4f9e4609f7d65c1
krouma <kroupa.matyas@gmail.com>
2017-03-23 20:51:32 +0100

    Hide flash messages after 5 seconds

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,11 @@
+//Init Ajax
 $(function () {
     $.nette.init();
+});
+
+//Hide flash messages after 5 seconds
+$(document).ready(function () {
+    setTimeout(function () {
+        $('.alert').fadeOut('fast');
+    }, 5000); // <-- time in milliseconds
 });
